22f8d9bd7ab5e94179ea61b66d157fc79b12d52c
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2020-05-12 23:22:30 +0530

    Add files via upload

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_back_or user
+    else
+      # Create an error message and re-render the signin form
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+end
