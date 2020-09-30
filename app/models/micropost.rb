30a6ded0b33700eb1c78ae95c0b164481a1ed5b8
tamoghnatb <56554306+tamoghnatb@users.noreply.github.com>
2020-10-01 01:15:31 +0530

    Update micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,8 +5,11 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140}
 
   # Returns microposts from the users being followed by the given user
-<!--Paste here-->
-    
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships 
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
   
   end
 end
