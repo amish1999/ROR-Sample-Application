ed92e2755c22241942ee42c5c7c752f2429e9a68
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2022-10-01 00:03:26 +0530

    Update microposts_controller.rb

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -13,19 +13,12 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy
-    @micropost.destroy
-    redirect_to root_url
-  end
-
-  private
-    
-    def micropost_params
-      params.require(:micropost).permit(:content)
-    end
-
-    def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
-    end
+//////
+  
+  
+  
+  
+  
+  
+  
 end
